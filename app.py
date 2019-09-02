e5cb47db94b7b286d71f469b7f4c1570fef796db
Ryan Keys <r.keys1998@gmail.com>
2019-09-02 15:37:35 -0700

    changed format of story str to reflect used functions

--- app.py
+++ app.py
@@ -35,24 +35,28 @@ def verbs():
 
 # Inputs random list item into story based on int values assigned to functions
 # Story from http://www.madlibs.com/printables/
+adjectives = adjectives()
+nouns = nouns()
+verbs = verbs()
+
 
 def story():
-    return '''A vacation is when you take a trip to some {0} place
-    with your {0} family. Usually you go to some place
-    that is near a/an {0} or up on a/an {1}.
-    A good vacation place is one where you can ride {1}
-    or play {1} or go hunting for {1} . I like
-    to spend my time {1} or {2}.
+    return f'''A vacation is when you take a trip to some {adjectives} place
+    with your {adjectives} family. Usually you go to some place
+    that is near a/an {adjectives} or up on a/an {nouns}.
+    A good vacation place is one where you can ride {nouns}
+    or play {nouns} or go hunting for {nouns} . I like
+    to spend my time {nouns} or {verbs}.
     When parents go on a vacation, they spend their time eating
-    three {2} a day, and fathers play golf, and mothers
-    sit around {1}. Last summer, my little brother
-    fell in a/an {2} and got poison {2} all
-    over his {2}. My family is going to go to (the)
-    {2}, and I will practice {1}. Parents
+    three {verbs} a day, and fathers play golf, and mothers
+    sit around {nouns}. Last summer, my little brother
+    fell in a/an {verbs} and got poison {verbs} all
+    over his {verbs}. My family is going to go to (the)
+    {verbs}, and I will practice {nouns}. Parents
     need vacations more than kids because parents are always very
-    {0} and because they have to work {0}
-    hours every day all year making enough {2} to pay
-    for the vacation.'''.format(adjectives(), nouns(), verbs())
+    {adjectives} and because they have to work {adjectives}
+    hours every day all year making enough {verbs} to pay
+    for the vacation.'''
 
 
 def test():
